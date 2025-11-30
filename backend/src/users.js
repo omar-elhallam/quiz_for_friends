dd0a70b3d016ad0aa3cb350e2836142da77b06d8
omar-elhallam <omar.elhallam8@gmail.com>
2025-11-30 23:03:22 +0100

    update passwords

--- backend/src/users.js
+++ backend/src/users.js
@@ -12,13 +12,13 @@ const users = {
   // Player accounts - can only join and play
   // Add your friends' accounts here
   players: {
-    Reda: { username: 'Reda', password: 'pass1', role: 'player' },
+    Reda: { username: 'Reda', password: 'abarro8', role: 'player' },
     Abdel: { username: 'Abdel', password: 'pass2', role: 'player' },
-    Hamza: { username: 'Hamza', password: 'pass3', role: 'player' },
-    Bichr: { username: 'Bichr', password: 'pass4', role: 'player' },
-    Imrane: { username: 'Imrane', password: 'pass5', role: 'player' },
+    Hamza: { username: 'Hamza', password: 'caramel1', role: 'player' },
+    Bichr: { username: 'Bichr', password: 'demandephotomeuf', role: 'player' },
+    Imrane: { username: 'Imrane', password: 'pedophile', role: 'player' },
     Rayan: { username: 'Rayan', password: 'pass6', role: 'player' },
-    player7: { username: 'player7', password: 'pass7', role: 'player' },
+    player7: { username: 'ping', password: 'pong', role: 'player' },
     player8: { username: 'player8', password: 'pass8', role: 'player' }
   }
 };
